10c653825becd535747a553ebd88bdf740caee63
Prakhar Jain <theprakharjain@gmail.com>
2021-03-06 20:35:57 +0530

    Solved the medium array problem FIND THE DUPLICATE NUMBER-287-LEETCODE

---
+++ Array/find_the_duplicate_number.py
@@ -0,0 +1,21 @@
+# Find the Duplicate Number - Leetcode-Medium
+
+# Solution-1 -------------------------------------------
+
+nums = [1, 3, 4, 2, 2]
+
+thisdict = {}
+answer = 0
+
+for i in range(len(nums)):
+    if not thisdict:
+        thisdict.update({nums[i]:nums[i]})
+
+    if nums[i] != thisdict.get(nums[i]):
+        thisdict.update({nums[i]: nums[i]})
+    else:
+        answer = thisdict.get(nums[i])
+
+print(answer)
+
+
